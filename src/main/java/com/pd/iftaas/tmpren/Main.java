9157cf4f79012c6c1f0d43ed5df3db56a61cbfcc
Ralf Spöth <ralf.spoeth@profidata.com>
2023-03-11 14:21:36 +0100

    initial

---
+++ src/main/java/com/pd/iftaas/tmpren/Main.java
@@ -0,0 +1,44 @@
+package com.pd.iftaas.tmpren;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+public class Main {
+    public static void main(String[] args) {
+        var dir = Path.of(System.getProperty("user.dir"));
+        try {
+            var srcPattern = Pattern.compile(args[0]);
+            var linkPattern = args[1];
+            try (var dirContents = Files.list(dir)) {
+                dirContents.map(Path::getFileName).forEach(f -> {
+                            var m = srcPattern.matcher(f.toString());
+                            if (m.matches()) {
+                                var linkedFile = dir.resolve(m.replaceAll(linkPattern));
+                                try {
+                                    Files.createDirectories(linkedFile.getParent());
+                                    Files.createLink(linkedFile, dir.resolve(f));
+                                } catch (IOException e) {
+                                    throw new RuntimeException(e);
+                                }
+                            }
+                        }
+                );
+            }
+        } catch (IndexOutOfBoundsException | PatternSyntaxException e) {
+            usage();
+            e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void usage() {
+        System.out.println("Usage: tmpren <srcpattern> <linkexpr>");
+        System.out.println("Where:");
+        System.out.println("\tsrcpattern = regex expression for the source file, potentially containing groups");
+        System.out.println("\tlinkexpr = expression for the link file, potentially containing group refs");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,2 @@
+module com.pd.iftaas.tmpren {
+}
